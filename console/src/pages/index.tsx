dee67c20aef25f85cb69da647b9462fc37506b14
damaera <damaera@ruangguru.com>
2020-07-08 17:56:14 +0700

    autgen swagger client console

---
+++ console/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@chakra-ui/core";
+
+const IndexPage = () => (
+    <div>
+        <Button variantColor="blue">Hello button</Button>
+    </div>
+);
+
+export default IndexPage;
